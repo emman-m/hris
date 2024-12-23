b17a8db318862ca7b6262c64adac5cf8de4028db
Your Name <you@example.com>
2024-12-23 23:54:39 +0800

    Added database configuration and user table migration

--- app/Database/Migrations/2024-12-23-151136_Users.php
+++ app/Database/Migrations/2024-12-23-151136_Users.php
@@ -8,11 +8,75 @@ class Users extends Migration
 {
     public function up()
     {
-        //
+        $this->db->disableForeignKeyChecks();
+
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'status' => [
+                'type' => 'INT',
+                'constraint' => 1,
+                'default' => '1',
+                'comment' => '1:true, 0:false',
+            ],
+            'role' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20',
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_general_ci',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_general_ci',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_general_ci',
+            ],
+            'status' => [
+                'type' => 'VARCHAR',
+                'default' => 'active',
+                'constraint' => '50',
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_general_ci',
+            ],
+            'code' => [
+                'type' => 'VARCHAR',
+                'constraint' => '6',
+                'collation' => 'utf8mb4_general_ci',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+                'on_update' => 'CURRENT_TIMESTAMP',
+            ],
+            'deleted_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users', true);
+
+        $this->db->enableForeignKeyChecks();
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
