1fcb63ae2c7039d0569f4383748eae61cedb487b
Your Name <you@example.com>
2025-02-10 11:53:11 +0800

    Refactor user status switch functionality to include account disable confirmation and update error handling

--- public/js/users/switch.js
+++ public/js/users/switch.js
@@ -1,55 +1,74 @@
 $(function () {
     $('.status-switch').on('change', function () {
-        const url = $(this).data('url');
-        const user_id = $(this).data('id');
-        const status = $(this).prop('checked') ? 1 : 0;
-        console.log(status);
-        var data = {
-            status: status,
-            user_id: user_id
-        };
+        const state = $(this).prop('checked') ? 1 : 0;
+        
+        Swal.fire({
+            title: "Are you sure?",
+            text: "This may affect the user's status.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: `Yes ${state ? 'Enable' : 'Disable'} it!`, 
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const url = $(this).data('url');
+                const user_id = $(this).data('id');
 
-        data[csrfTokenName] = csrfTokenValue;
+                const status = state ? 1 : 0;
+                console.log(status);
+                var data = {
+                    status: status,
+                    user_id: user_id
+                };
 
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: data,
-            dataType: 'json',
-            beforeSend: function () {
-                $('.status-switch').prop('disabled', true);
-            },
-            success: function (response) {
-                // Refresh token
-                csrfTokenValue = response.csrfToken;
-                if (response.success) {
-                    $('.status-switch').prop('disabled', false);
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: "top-end",
-                        showConfirmButton: false,
-                        timer: 5000,
-                        timerProgressBar: true,
-                        showCloseButton: true,
-                        customClass: {
-                            popup: 'alert alert-success', // Add a custom class for the toast
-                        },
-                        didOpen: (toast) => {
-                            toast.onmouseenter = Swal.stopTimer;
-                            toast.onmouseleave = Swal.resumeTimer;
+                data[csrfTokenName] = csrfTokenValue;
+
+                $.ajax({
+                    url: url,
+                    type: 'POST',
+                    data: data,
+                    dataType: 'json',
+                    beforeSend: function () {
+                        $('.status-switch').prop('disabled', true);
+                    },
+                    success: function (response) {
+                        // Refresh token
+                        csrfTokenValue = response.csrfToken;
+                        if (response.success) {
+                            $('.status-switch').prop('disabled', false);
+                            const Toast = Swal.mixin({
+                                toast: true,
+                                position: "top-end",
+                                showConfirmButton: false,
+                                timer: 5000,
+                                timerProgressBar: true,
+                                showCloseButton: true,
+                                customClass: {
+                                    popup: 'alert alert-success', // Add a custom class for the toast
+                                },
+                                didOpen: (toast) => {
+                                    toast.onmouseenter = Swal.stopTimer;
+                                    toast.onmouseleave = Swal.resumeTimer;
+                                }
+                            });
+                            Toast.fire({
+                                icon: "success",
+                                text: response.message,
+                            });
+                            $('.status-switch').prop('disabled', false);
                         }
-                    });
-                    Toast.fire({
-                        icon: "success",
-                        text: response.message,
-                    });
-                    $('.status-switch').prop('disabled', false);
-                }
-            },
-            error: function (err) {
-                $('.status-switch').prop('disabled', false);
-                console.error(err);
+                    },
+                    error: function (err) {
+                        $('.status-switch').prop('disabled', false);
+                        console.error(err);
+                    }
+                })
+            } else {
+                $(this).prop('checked', !state);
             }
-        })
+        });
+
+
     })
 });
